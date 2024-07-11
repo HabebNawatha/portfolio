5606f4e93f72813bfbfbeb764e417ad397a86f79
Habeb Nawatha <109216430+HabebNawatha@users.noreply.github.com>
2024-07-11 14:37:58 +0300

    Edit Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 import WaveAnimation from '../../Assets/waves/WaveAnimation';
 import WaveEmoji from '../../Assets/waves/WaveEmoji';
 import Type from '../../Assets/typewriter/Type';
 import RightImage from '../../Assets/images/pic1.png';
-import { TfiEmail } from "react-icons/tfi";
-
+import resume from '../../Assets/files/resume.pdf';
+import { AiOutlineDownload } from 'react-icons/ai';
 
 function Home() {
+  const handleDownloadResume = () => {
+    window.open(resume, "_blank");
+  };
+
   return (
     <div className='home-section'>
       <div className='home-grid'>
@@ -26,6 +31,9 @@ function Home() {
           </div>
           <div className='quote-div'>
             <h5>Programmer: A machine that turns coffee into code.</h5>
+            <a href={resume} download className='btn'>
+              <AiOutlineDownload /> Download CV
+            </a>
           </div>
         </div>
         <div className='right-grid'>
