04bb386d68ef530f38479a384f3a83389b08ef98
Habeb Nawatha <109216430+HabebNawatha@users.noreply.github.com>
2024-07-11 17:20:13 +0300

    Edit footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import './Footer.css'; // Make sure to create and import the CSS file for styling
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import './Footer.css';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-section">
-          <p>Designed and Developed by Habeb</p>
+          <p>Designed and Developed by Habeb Nawatha</p>
         </div>
         <div className="footer-section-social">
-          <span>
-          <a href="https://www.linkedin.com/in/habeb-nawatha/" className='footer-icon'><LinkedInIcon/></a>
-          </span>
-          <span>
-          <a href="https://github.com/HabebNawatha" className='footer-icon'><GitHubIcon/></a>
-          </span>
+          <a href="https://www.linkedin.com/in/habeb-nawatha/" className='footer-icon'><FaLinkedin/></a>
+          <a href="https://github.com/HabebNawatha" className='footer-icon'><FaGithub/></a>
+          <a href="mailto:habeb.naw@gmail.com" className='footer-icon'><FaEnvelope/></a>
         </div>
       </div>
     </footer>
