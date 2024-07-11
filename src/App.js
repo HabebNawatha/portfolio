5d4a1be811e2f4a575a28b0426d823c744f2dc40
Habeb Nawatha <109216430+HabebNawatha@users.noreply.github.com>
2024-07-11 11:28:30 +0300

    changes made to style

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
 import About from "./components/About/About.js";
 import ScrollToTop from "./components/ScrollToTop";
+import Resume from "./components/Resume/Resume.js";
 
 import {BrowserRouter as Router,Route,Routes,Navigate} from "react-router-dom";
 
@@ -31,7 +32,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/project" element={<Projects />} />
             <Route path="/about" element={<About />} />
-            {/* <Route path="/resume" element={<Resume />} /> */}
+            <Route path="/resume" element={<Resume />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </div>
