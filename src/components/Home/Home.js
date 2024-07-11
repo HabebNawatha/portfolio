2fd5cc8ee695c9f07b80d1948a32bc6deda3c0a4
Habeb Nawatha <109216430+HabebNawatha@users.noreply.github.com>
2024-07-11 19:03:45 +0300

    Edit in Home section, cv download button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Home.css';
-import WaveAnimation from '../../Assets/waves/WaveAnimation';
 import WaveEmoji from '../../Assets/waves/WaveEmoji';
 import Type from '../../Assets/typewriter/Type';
 import RightImage from '../../Assets/images/pic1.png';
-import resume from '../../Assets/files/resume.pdf';
-import { AiOutlineDownload } from 'react-icons/ai';
+import ResumeBtn from "../../Assets/ResumeBtn/ResumeBtn.js";
 
 function Home() {
-  const handleDownloadResume = () => {
-    window.open(resume, "_blank");
-  };
 
   return (
     <div className='home-section'>
@@ -31,9 +25,7 @@ function Home() {
           </div>
           <div className='quote-div'>
             <h5>Programmer: A machine that turns coffee into code.</h5>
-            <a href={resume} download className='btn'>
-              <AiOutlineDownload /> Download CV
-            </a>
+            <ResumeBtn/>
           </div>
         </div>
         <div className='right-grid'>
